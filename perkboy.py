6d44c4990bb14d863c1c535e9c705c9ab8b1fa89
neonowl jerm <jerm@neonowl.net>
2019-02-07 11:19:28 -0500

    Parse perk name function, removed blank lines

--- perkboy.py
+++ perkboy.py
@@ -10,27 +10,36 @@ def souper(url):
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
     return soup
 
+# Parse SPECIAL stat names
 def get_stat_names(soup_object):
     stats = []
-    
     stat_names = soup_object.find_all("span", class_="mw-headline")
     del stat_names[0:2]
-
     for name in stat_names:
         stats.append(name.text)
-
     return stats
 
+# Parse the perk names
+def get_perk_names(soup_object):
+    perks = []
+    perk_table = soup_object("table")[5:12]
+    for table in perk_table:
+        perk_names = table.find_all("a")
+        for name in perk_names:
+            perks.append(name.text)
+    return perks
 
 soup = souper('https://fallout.gamepedia.com/Fallout_76_perks')
 
 SPECIAL = get_stat_names(soup)
 
-for name in SPECIAL:
-    print(name)
+all_perks = get_perk_names(soup)
+
+for perk in all_perks:
+    print(perk)
 
 # TO-DO
 # 
-# Parse the perks
-# Display the perks
-# Store the perks in a file
+# List perks by their SPECIAL stat
+# Display perk info
+# Write perks to a file
